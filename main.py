4df9b243b035288c26d8686b1dc0cf3071ff21a7
dancarrillo98 <dancarrillo98@gmail.com>
2021-12-27 13:34:09 -0600

    Code passed through pylint

--- main.py
+++ main.py
@@ -1,75 +1,85 @@
+'''
+Main.py has two games that users can choose from and play. The first is
+dice roll and the second is guess the number. Enjoy!
+'''
 import random
 import time
 
-
-class Games():
-
-  # GAME 1: Dice roll
-  def dice_roll():
-    Games.loading("Rolling Dice 1...")
-    rand_num = random.randint(1,6)
-    print(f"Dice 1: {rand_num}\n")
-
-    Games.loading("Rolling Dice 2...")
-    rand_num2 = random.randint(1,6)
-    print(f"Dice 2: {rand_num2}\n")
-
-    Games.loading("Calculating total...")
-    total = rand_num + rand_num2
-    print(f"Roll total: {total}\n")
-    Games.loading("...")
-    return total
-
-
-  # GAME 2: Guess the number
-  def guess_number():
-    Games.loading("Choosing a random number between 1 and 20...")
-    randomNum = random.randint(1, 20)
-    guessCount = 0
-
-    while guessCount < 5:
-      Games.loading(f"You have {5 - guessCount} guesses remaining...")
-      userInput = int(input("What's your guess? "))
-      if userInput > 20 or userInput < 1:
-        Games.loading("Please provide a number between 1 and 20...")
-      else:
-        if userInput == randomNum:
-          Games.loading(f"You guessed CORRECT! The number was {randomNum}")
-          return randomNum
-        else:
-          Games.loading(f"You guess is wrong! Please try again")
-          guessCount += 1
-      
-    Games.loading("You have used all your guesses. Better luck next time!")
-    return randomNum
-      
-
-  # Loading text with 1 sec delay
-  def loading(text):
+def loading(text):
+    '''
+    Loads text with a one second delay!
+    '''
     print(text)
     time.sleep(1)
 
+class Games():
+    '''
+    The games class creates an object capable of two games: dice roll and
+    guess the number
+    '''
+    @staticmethod
+    def dice_roll():
+        '''
+        Game #1: Dice Roll!
+        '''
+        loading("Rolling Dice 1...")
+        rand_num = random.randint(1,6)
+        print(f"Dice 1: {rand_num}\n")
+
+        loading("Rolling Dice 2...")
+        rand_num2 = random.randint(1,6)
+        print(f"Dice 2: {rand_num2}\n")
+
+        loading("Calculating total...")
+        total = rand_num + rand_num2
+        print(f"Roll total: {total}\n")
+        loading("...")
+        return total
+
+    @staticmethod
+    def guess_number():
+        '''
+        Game #2: Guess the number!
+        '''
+        loading("Choosing a random number between 1 and 20...")
+        random_num = random.randint(1, 20)
+        guess_count = 0
+
+        while guess_count < 5:
+            loading(f"You have {5 - guess_count} guesses remaining...")
+            user_input = int(input("What's your guess? "))
+            if user_input > 20 or user_input < 1:
+                loading("Please provide a number between 1 and 20...")
+            else:
+                if user_input != random_num:
+                    loading("You guess is wrong! Please try again")
+                    guess_count += 1
+                else:
+                    loading(f"You guessed CORRECT! The number was {random_num}")
+                    return random_num
+        loading("You have used all your guesses. Better luck next time!")
+        return random_num
 # end class
 
 
 # CLI MENU
-gameOn = True
+GAME_ON = True
 
-while gameOn:
+while GAME_ON:
 
-  #User Interface
-  print("Welcome to 2N1. \n")
-  print("Press [1] for a dice roll.\n")
-  print("Press [2] for the Number Guess game.\n")
-  print("Press [0] to exit")
+    #User Interface
+    print("Welcome to 2N1. \n")
+    print("Press [1] for a dice roll.\n")
+    print("Press [2] for the Number Guess game.\n")
+    print("Press [0] to exit")
 
-  user_resp = int(input("Which would you like to select? "))
-  if user_resp == 1:
-    Games.dice_roll()
-  elif user_resp == 2:
-    Games.guess_number()
-  elif user_resp == 0:
-    Games.loading("Exiting...")
-    gameOn = False
-  elif user_resp < 0 or user_resp >= 3:
-    Games.loading("Please select 1, 2 or 0")
+    user_resp = int(input("Which would you like to select? "))
+    if user_resp == 1:
+        Games.dice_roll()
+    elif user_resp == 2:
+        Games.guess_number()
+    elif user_resp == 0:
+        loading("Exiting...")
+        GAME_ON = False
+    elif user_resp < 0 or user_resp >= 3:
+        loading("Please select 1, 2 or 0")
